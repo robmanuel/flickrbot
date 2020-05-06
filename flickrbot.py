f3aad60881891834ba7e9b668e7ae59289a79643
robmanuel <robmanuel@gmail.com>
2020-05-06 11:38:43 +0100

    Update flickrbot.py

--- flickrbot.py
+++ flickrbot.py
@@ -1,16 +1,28 @@
 # -*- coding: utf-8 -*-
 
-# FlickrBot 0.2
+# FlickrBot 0.3
 # 
 # This code will pick a random photo from a Flickr user and post it on Twitter
 # 
 # Made by Rob Manuel from a request by Paul Clarke
 #
+# USE: 
+# python flickrbot.py -tag paulfav -id paul_clarke -nocontext 
+#
+# install
+# git clone https://github.com/robmanuel/flickrbot
+#
+# version 0.3 adds a default privacy setting of 1
+#
+# 1 public photos
+# 2 private photos visible to friends
+# 3 private photos visible to family
+# 4 private photos visible to friends & family
+# 5 completely private photos
+#
 # version 0.2 moves api keys to an ini file
 # and allows tag searching
-# e.g.
-# python flickrbot.py -tag paulfav -id paul_clarke -nocontext 
-#
+
 
 import flickr_api,random,tweepy,argparse
 
@@ -35,11 +47,14 @@ parser = argparse.ArgumentParser(description='Posts random Flickr image on Twitt
 parser.add_argument('-id',required=False,default=id,type=str,help='ID of Flickr user')
 parser.add_argument('-nocontext', action='store_true',help="send tweet without any text if true")
 parser.add_argument('-tag',required=False,type=str,help='Tag to search for')
+parser.add_argument('-privacy_filter',required=False,type=str,help='Return photos only matching a certain privacy level',default="1")
+
 
 args = vars(parser.parse_args())
 
 id=args["id"] 
 tag=args["tag"]
+privacy_filter=args["privacy_filter"]
 
 flickr_api.set_keys(api_key = api_key, api_secret = api_secret)
 user=flickr_api.Person.findByUserName(id)
@@ -47,9 +62,9 @@ user=flickr_api.Person.findByUserName(id)
 
 if tag is None:
     user=flickr_api.Person.findByUserName(id)
-    photos = user.getPhotos()
+    photos = user.getPhotos(privacy_filter=privacy_filter)
 else:
-    photos=flickr_api.Photo.search(tags=tag,user_id=id) 
+    photos=flickr_api.Photo.search(tags=tag,user_id=id,privacy_filter=privacy_filter) 
 
 pagetot=photos.info.pages
 
@@ -60,9 +75,9 @@ page=random.randrange(0,pagetot)
 
 print "We're retrieving page: %s" % (page)
 if tag is None:
-    photos = user.getPhotos(page=page)
+    photos = user.getPhotos(page=page,privacy_filter=privacy_filter)
 else:
-    photos = flickr_api.Photo.search(tags=tag,page=page,user_id=id)
+    photos = flickr_api.Photo.search(tags=tag,page=page,user_id=id,privacy_filter=privacy_filter)
 
 phot=random.randrange(0,len(photos))
 url="https://www.flickr.com/photos/%s/%s" % (id,photos[phot].id)
